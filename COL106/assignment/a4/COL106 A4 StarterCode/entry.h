9a5ee560e93bf3c2721c341e26af10ade686b402
logxdx <iamvarun158@gmail.com>
2023-09-19 20:24:33 +0530

    Starter Code

---
+++ COL106/assignment/a4/COL106 A4 StarterCode/entry.h
@@ -0,0 +1,16 @@
+#include "string"
+#include "ulimitedrational.h"
+using namespace std;
+
+class SymEntry {
+public:
+    string key;
+    UnlimitedRational* val;
+    
+    SymEntry* left;
+    SymEntry* right;
+
+    SymEntry();
+    SymEntry(string k, UnlimitedRational* v);
+    
+};
