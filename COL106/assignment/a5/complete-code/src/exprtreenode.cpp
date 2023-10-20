feba97aecf4ddd03f0a6daf09d89ab8c0e12f297
logxdx <iamvarun158@gmail.com>
2023-10-20 12:01:58 +0530

    Expression Tree Node - complete and working

--- COL106/assignment/a5/complete-code/src/exprtreenode.cpp
+++ COL106/assignment/a5/complete-code/src/exprtreenode.cpp
@@ -5,14 +5,33 @@
 //Write your code below this line
 
 ExprTreeNode::ExprTreeNode(){
-
+    type = "";
+    id = "";
+    num = 0;
 }
 
-ExprTreeNode::ExprTreeNode(string t,int v){
-  
+ExprTreeNode::ExprTreeNode(string t, int v){
+    
+    id = "";
+    
+    if (t == "+") {type = "ADD"; id = "+";}
+    else if (t == "-") {type = "SUB"; id = "-";}
+    else if (t == "*") {type = "MUL"; id = "*";}
+    else if (t == "/") {type = "DIV"; id = "/";}
+    else if (t == "VAL") {type = "VAL";}
+    else if (t == "=") {type = "ROOT"; id = "=";}
+    else if (t == "del") {type = "DEL";}
+    else if (t == "ret") {type = "RET";}
+    else {type = "VAR"; id = t;}
+
+    num = v;
+    left = NULL;
+    right = NULL;
+
 }
 
 ExprTreeNode::~ExprTreeNode(){
-
+    delete left;
+    delete right;
 }
 
