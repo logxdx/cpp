250daa47e0449479f53ffcc3cc47dfa090d1bc5f
logxdx <iamvarun158@gmail.com>
2023-10-20 12:00:40 +0530

    Complete and working

--- COL106/assignment/a5/complete-code/src/symnode.cpp
+++ COL106/assignment/a5/complete-code/src/symnode.cpp
@@ -6,25 +6,57 @@
 
 SymNode::SymNode(){
 
+    key = "";
+    height = 1;
+
 }
 
 SymNode::SymNode(string k){
+
+    key = k;
+    height = 1;
 
 }
 
 SymNode* SymNode::LeftLeftRotation(){
 
+    SymNode* newRoot = right;
+
+    right = newRoot->left;
+    newRoot->left = this;
+
+    height = max((left ? left->height : 0), (right ? right->height : 0)) + 1;
+    newRoot->height = max((newRoot->left ? newRoot->left->height : 0), (newRoot->right ? newRoot->right->height : 0)) + 1;
+
+    return newRoot;
+
 }
 
 SymNode* SymNode::RightRightRotation(){
 
+    SymNode* newRoot = left;
+    
+    left = newRoot->right;
+    newRoot->right = this;
+
+    height = max((left ? left->height : 0), (right ? right->height : 0)) + 1;
+    newRoot->height = max((newRoot->left ? newRoot->left->height : 0), (newRoot->right ? newRoot->right->height : 0)) + 1;
+
+    return newRoot;
+    
 }
 
 SymNode* SymNode::LeftRightRotation(){
 
+    left = left->LeftLeftRotation();
+    return RightRightRotation();
+
 }
 
 SymNode* SymNode::RightLeftRotation(){
+    
+    right = right->RightRightRotation();
+    return LeftLeftRotation();
 
 }
 
