cf618444409585d120e4fc402ca7e470efc9b294
logxdx <iamvarun158@gmail.com>
2023-10-21 16:23:44 +0530

    Private attributes added - TreeNode class, AVL Tree class

--- COL106/assignment/a6/A6 Startercode/dict.h
+++ COL106/assignment/a6/A6 Startercode/dict.h
@@ -8,6 +8,80 @@ using namespace std;
 class Dict {
 private:
     // You can add attributes/helper functions here
+    
+    class TreeNode {
+        
+        public:
+        
+            string key;
+            int height;
+            int count=0;
+            
+            TreeNode* left = NULL;
+            TreeNode* right = NULL;
+
+            TreeNode();
+            TreeNode(string k);
+
+            TreeNode* LeftLeftRotation(); // case 1
+            TreeNode* RightRightRotation(); // case 2
+            TreeNode* LeftRightRotation(); // case 3
+            TreeNode* RightLeftRotation(); // case 4
+
+            ~TreeNode();
+            
+        };
+
+    class AVL_TREE {
+
+        public:
+
+            int size = 0;
+            TreeNode* root = NULL;
+
+            AVL_TREE();
+
+            // Insert a key in the symbol table, there is no value now
+            Dict::TreeNode* insert_recursive(string k, Dict::TreeNode* node);
+            void insert(string k);
+
+            // Remove a key (and it's memory address) in the symbol table
+            Dict::TreeNode* remove_recursive(string k, Dict::TreeNode* node);
+            void remove(string k);
+        
+            // Find the memory address corresponding to the variable in the symbol table
+            int search(string k);
+
+            // Pretty print Tree
+            void print_AVL_Tree(TreeNode* node, int depth = 0);
+            void write_AVL_Tree(TreeNode* node, vector<string> &words);
+
+            // Get size
+            int get_size();
+
+            // Get height
+            int getHeight(Dict::TreeNode* node);
+
+            // Height(left tree) - Height(right tree)
+            int getBalance(Dict::TreeNode* node);
+
+            // Balances the tree
+            Dict::TreeNode* balancer(Dict::TreeNode* node); 
+
+            // Get the minimum element
+            Dict::TreeNode* findMin(Dict::TreeNode* node);
+
+            // Get root 
+            TreeNode* get_root();
+
+            // Helper function to delete tree
+            void delete_tree(TreeNode* node);
+
+            ~AVL_TREE();
+            
+        };    
+
+    AVL_TREE* tree;
 
 public: 
     /* Please do not touch the attributes and 
